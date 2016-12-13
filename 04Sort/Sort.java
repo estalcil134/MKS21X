540cf434334473fe4ab2e91eec352a42a3795faa
estalcil134 <wilsonwong1688@gmail.com>
2016-12-13 15:32:21 -0500

    Update Sort.java
    started swap, selectionSwap

--- 04Sort/Sort.java
+++ 04Sort/Sort.java
@@ -1,1 +1,15 @@
 public class Sort{
+  public static String name(){
+    return "10.Wong.Wilson";
+  }
+  
+  public static void swap(int[] data, int a, int b){
+    int c = data[a];
+    data[a] = data[b];
+    data[b] = c;
+  }
+  
+  public static void selectionSort(int[] data){
+    int end = data.length;
+    for(int start = 0; start < end; start + 1){
+      
